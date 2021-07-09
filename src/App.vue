181c09d615eca3bb8f930fcc973051f8c9f2a719
tangyu1 <tangyu1@orientsec.com.cn>
2021-07-09 09:52:29 +0800

    init project

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <ExhibitionHall msg="" />
+</template>
+
+<script>
+import ExhibitionHall from './components/ExhibitionHall.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ExhibitionHall
+  }
+}
+</script>
---
+++ src/main.js
@@ -0,0 +1,12 @@
+/*
+ * @Author: tangyu
+ * @Date: 2021-07-08 17:05:43
+ * @Company: orientsec.com.cn
+ * @Description: 
+ */
+import { createApp } from 'vue'
+import App from './App.vue'
+import './index.css'
+
+createApp(App).mount('#app')
+console.log(import.meta.env);
